e11ebcff7cd8bdab8a31388ffbe559f0f5b267ad
Nikkyie <hirangcloyd@gmail.com>
2026-02-21 06:20:56 +0800

    added: home and app showcases css changes for mobile responsiveness

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,13 +6,13 @@ const Home = () => {
     <section className="home section" id="home">
       <div className="home__container container grid">
         <div className="home__content">
+          <div className="home__img"></div>
           <div className="home__introduction">
             <h1 className="home__title">Hi, I'm Cloyd</h1>
             <h3 className="home__subtitle">Application Developer</h3>
             <p className="home__description">I create cross-platform apps for Android and iOS. I also have experience in ERP systems development.</p>
             <a href="#contact" className="button button--flex">Contact Me</a>
           </div>
-          <div className="home__img"></div>
         </div>
       </div>
     </section>
